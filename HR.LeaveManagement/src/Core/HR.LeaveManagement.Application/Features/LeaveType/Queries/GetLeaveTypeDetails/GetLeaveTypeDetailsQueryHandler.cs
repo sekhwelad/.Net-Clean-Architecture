a8bbbb1f576c70634a652fa8efc0c3e6e4283cb5
213299298 <25sekhwelad@gmail.com>
2024-02-18 09:49:07 +0200

    CQRS Queries

---
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypeDetailsQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+        {
+            // Query the database
+            var leaveTypes = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            // Convert data objects to DTO objects
+            var result = _mapper.Map<LeaveTypeDetailsDto>(leaveTypes);
+
+            // return list of DTO
+            return result;
+        }
+    }
+}
